d644c07218a68d19c66f6872b39529aaf8b44f30
Manas Jain <109158799+manas3212@users.noreply.github.com>
2023-11-30 11:42:25 +0530

    Update producerconsumer.cpp

--- producerconsumer.cpp
+++ producerconsumer.cpp
@@ -3,102 +3,95 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
-#include <time.h>   
 
-#define NUM_PRODUCERS 4
-#define NUM_CONSUMERS 3
-#define N 5
-#define NUM_ITEMS 5
-sem_t mutex;
-sem_t empty;
-sem_t full;
-int buffer[N];
-int in = 0;
-int out = 0;
-int items_produced = 0;
-int items_consumed = 0;
-void displayBuffer()
-{
-    printf("\n---------------Buffer: [");
-    for (int i = 0; i < N; i++)
-    {
+#define BUFFER_SIZE 5
+#define NUM_PRODUCERS 3
+#define NUM_CONSUMERS 2
+#define NUM_ITEMS 10
+
+sem_t empty, full, mutex;
+int buffer[BUFFER_SIZE];
+int in = 0, out = 0;
+
+void displayBuffer() {
+    printf("Buffer: [");
+    for (int i = 0; i < BUFFER_SIZE; i++) {
         printf("%d ", buffer[i]);
     }
     printf("]\n");
 }
-void *producer(void *arg)
-{
+
+void *producer(void *arg) {
     int item;
-    while (items_produced < NUM_ITEMS)
-    {
+    for (int i = 0; i < NUM_ITEMS; i++) {
         item = rand() % 100;
-        displayBuffer();
-        printf("Producer is trying to produce item %d\n", item);
+
         sem_wait(&empty);
         sem_wait(&mutex);
-        if (((in + 1) % N) == out)
-        {
-            printf("\n ---------------The buffer is full---------------------\n");
-            displayBuffer();
-        }
+
         buffer[in] = item;
-        in = (in + 1) % N;
+        in = (in + 1) % BUFFER_SIZE;
+
         printf("Producer produced item %d\n", item);
-        items_produced++;
+        displayBuffer();
+
         sem_post(&mutex);
         sem_post(&full);
-    }
-    pthread_exit(NULL);
-}
-void *consumer(void *arg)
-{
-    int item;
-    while (item != -1)
-    {
-        sem_wait(&full);
-        sem_wait(&mutex);
-        item = buffer[out];
-        buffer[out] = -1;
-        out = (out + 1) % N;
-        printf("Consumer consumed item %d\n", item);
-        items_consumed++;
-        if (in == out)
-        {
-            printf("\n ---------------The buffer is Empty---------------------\n");
-            displayBuffer();
-        }
-        sem_post(&mutex);
-        sem_post(&empty);
+
         usleep(rand() % 100000);
     }
     pthread_exit(NULL);
 }
-int main()
-{
+
+void *consumer(void *arg) {
+    int item;
+    for (int i = 0; i < NUM_ITEMS; i++) {
+        sem_wait(&full);
+        sem_wait(&mutex);
+
+        item = buffer[out];
+        out = (out + 1) % BUFFER_SIZE;
+
+        printf("Consumer consumed item %d\n", item);
+        displayBuffer();
+
+        sem_post(&mutex);
+        sem_post(&empty);
+
+        usleep(rand() % 100000);
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
     srand(time(NULL));
-    sem_init(&mutex, 0, 1);
-    sem_init(&empty, 0, N);
+
+    sem_init(&empty, 0, BUFFER_SIZE);
     sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+
     pthread_t producer_threads[NUM_PRODUCERS];
     pthread_t consumer_threads[NUM_CONSUMERS];
-    for (int i = 0; i < NUM_PRODUCERS; i++)
-    {
+
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
         pthread_create(&producer_threads[i], NULL, producer, NULL);
     }
-    for (int i = 0; i < NUM_CONSUMERS; i++)
-    {
+
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
         pthread_create(&consumer_threads[i], NULL, consumer, NULL);
     }
-    for (int i = 0; i < NUM_PRODUCERS; i++)
-    {
+
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
         pthread_join(producer_threads[i], NULL);
     }
-    for (int i = 0; i < NUM_CONSUMERS; i++)
-    {
+
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
         pthread_join(consumer_threads[i], NULL);
     }
-    sem_destroy(&mutex);
+
     sem_destroy(&empty);
     sem_destroy(&full);
+    sem_destroy(&mutex);
+
     return 0;
 }
